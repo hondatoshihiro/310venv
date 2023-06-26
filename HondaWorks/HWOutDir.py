22f9f6b2b1d8b33f0e99bde2352f9bd722e0ef5d
hondatoshihiro <harutokaho@gmail.com>
2023-06-26 22:48:34 +0900

    クラス構成変更

--- HondaWorks/HWOutDir.py
+++ HondaWorks/HWOutDir.py
@@ -1,3 +1,9 @@
 import os
+import HWTargetDir
 
-class HWOutDir:
+class HWOutDir(HWTargetDir):
+
+    def __init__(self, filePath):
+        #HWTargetDirのコンストラクタ
+        super().__init__(filePath)
+        #独自のコンストラクタの処理
